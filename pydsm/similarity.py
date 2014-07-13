22e5ee6465a0069f5219e46d9bf0d6f1a31c6ffe
Jimmy Callin <jimmy.callin@gmail.com>
2014-07-13 21:32:01 +0200

    fixes

--- pydsm/similarity.py
+++ pydsm/similarity.py
@@ -3,44 +3,40 @@ from pydsm.matrix import Matrix
 import pydsm.model
 
 
-def _assure_consistency(dsm, vector):
+def _assure_consistency(matrix, vector):
     if not vector.is_vector():
         raise ValueError("Vector should have the shape (1,n).")
 
-    vector = vector.synchronize_word_order(dsm, axis=1)
-    if dsm.col2word != vector.col2word:
+    vector = vector.synchronize_word_order(matrix, axis=1)
+    if matrix.col2word != vector.col2word:
         raise ValueError("Columns of each matrix must match")
     return vector
 
-def dot(dsm, vector):
+def dot(matrix, vector):
     """
-    Calculate dot product distance for all words in dsm against vector.
+    Calculate dot product distance for all words in matrix against vector.
     """
-    vector = _assure_consistency(dsm, vector)
-    return dsm.matrix.dot(vector.transpose())
+    vector = _assure_consistency(matrix, vector)
+    return matrix.dot(vector.transpose())
 
 
-def euclidean(dsm, vector):
+def euclidean(matrix, vector):
     """
-    Calculate inversed euclidean distance for all words in dsm against vector.
+    Calculate inversed euclidean distance for all words in matrix against vector.
     """
-    vector = _assure_consistency(dsm, vector)
-    return 1/(1+dsm.matrix.subtract(vector).norm(axis=1))
+    vector = _assure_consistency(matrix, vector)
+    return 1/(1+matrix.subtract(vector).norm(axis=1))
 
 
-def cos(dsm, vector):
+def cos(matrix, vector):
     """
-    Calculate cosine distance for all words in dsm against all words in vector.
+    Calculate cosine distance for all words in matrix against all words in vector.
     """
-    vector = _assure_consistency(dsm, vector)
+    vector = _assure_consistency(matrix, vector)
 
-    if isinstance(dsm, pydsm.model.DSM):
-        mat1 = dsm.matrix
-    elif isinstance(dsm, Matrix):
-        mat1 = dsm
 
-    dotted = mat1.dot(vector.transpose())
-    mat1_norms = mat1.multiply(mat1).sum(axis=1).sqrt()
+    dotted = matrix.dot(vector.transpose())
+    mat1_norms = matrix.multiply(matrix).sum(axis=1).sqrt()
     mat2_norms = vector.multiply(vector).sum(axis=1).sqrt()
     mat1_mat2_norms = mat1_norms.multiply(mat2_norms)
     neighbors = dotted.multiply(1 / mat1_mat2_norms)
@@ -58,7 +54,7 @@ def nearest_neighbors(dsm, arg, simfunc=cos):
 
     scores = []
     for row in vec:
-        scores.append(simfunc(dsm, row).sort(key='sum', axis=0, ascending=False))
+        scores.append(simfunc(dsm.matrix, row).sort(key='sum', axis=0, ascending=False))
 
 
     res = scores[0]
