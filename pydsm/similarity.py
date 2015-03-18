788829d7df2cfd71ad496d1bd6a983941ab83554
Jimmy Callin <jimmy.callin@gmail.com>
2015-03-18 14:48:13 +0100

    fix matrix similarity when matrix is vector

--- pydsm/similarity.py
+++ pydsm/similarity.py
@@ -6,7 +6,7 @@ import pydsm.model
 def _assure_consistency(matrix, vector):
     return vector.synchronize_word_order(matrix, axis=1)
 
-def dot(matrix, vector, assure_consistency=True):
+def dot(matrix, vector, assure_consistency=False):
     """
     Calculate dot product distance for all words in matrix against vector.
     """
@@ -15,7 +15,7 @@ def dot(matrix, vector, assure_consistency=True):
     return matrix.dot(vector.transpose())
 
 
-def euclidean(matrix, vector, assure_consistency=True):
+def euclidean(matrix, vector, assure_consistency=False):
     """
     Calculate inversed euclidean distance for all words in matrix against vector.
     """
@@ -25,7 +25,7 @@ def euclidean(matrix, vector, assure_consistency=True):
     return inv_euc.sort(ascending=False)
 
 
-def cos(matrix, vector, assure_consistency=True):
+def cos(mat1, mat2, assure_consistency=False):
     """
     Calculate cosine distance for all words in matrix against all words in vector.
     Params:
@@ -36,17 +36,18 @@ def cos(matrix, vector, assure_consistency=True):
     """
 
     if assure_consistency:
-        vector = _assure_consistency(matrix, vector)
+        mat2 = _assure_consistency(mat1, mat2)
 
-    if matrix.is_vector():
-        return _vector_vector_cos(matrix, vector)
+    if mat1.is_vector() and mat2.is_vector():
+        return _vector_vector_cos(mat1, mat2)
+        
 
-    matrix = matrix / matrix.norm(axis=1)
-    vector = vector / vector.norm(axis=1)
-    dotted = matrix.dot(vector.transpose())
+    mat1 = mat1 / mat1.norm(axis=1)
+    mat2 = mat2 / mat2.norm(axis=1)
+    dotted = mat1.dot(mat2.transpose())
     return dotted
 
-def _vector_vector_cos(v1, v2, assure_consistency=True):
+def _vector_vector_cos(v1, v2, assure_consistency=False):
     """
     Faster calculation for vector pair similarity.
     """
